9bcae51ef7b2318da1b0a54b51bf6d7b3bbdf495
Lucsmtsoares <lmsl3@aluno.ifal.edu.br>
2024-03-12 02:24:23 -0300

    teste funcional[7]

--- api/run.py
+++ api/run.py
@@ -1,4 +1,18 @@
-from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(): 
+    return render_template("basic.html")
+
+
+@app.route("/processing", methods=['POST'])
+def analisar_portarias():
+    from models.teste import imprimir
+    c = imprimir()
+        
+    return render_template("teste.html", c=c)
 
 
 if __name__ == "__main__":
