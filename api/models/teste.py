c1ac12215a8e903d0fec39e2c4bf3dcf427397a6
Lucsmtsoares <lmsl3@aluno.ifal.edu.br>
2024-03-16 23:20:05 -0300

    Funcionando

--- api/models/teste.py
+++ api/models/teste.py
@@ -7,12 +7,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+from selenium.webdriver import DesiredCapabilities, Remote
+from webdriver_manager.chrome import ChromeDriverManager
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')  # Se você quiser visualizar o navegador, remova esta linha
-driver = webdriver.Chrome(options=options)
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+options = ChromeOptions()
+options.browser_version = 'latest'
+options.platform_name = 'Windows 11'
+sauce_options = {}
+sauce_options['username'] = 'oauth-lucasoares3243-19726'
+sauce_options['accessKey'] = '9a6220d2-0c79-4375-98ee-b2a598127379'
+sauce_options['build'] = 'selenium-build-17DEX'
+sauce_options['name'] = 'Teste-dou'
+options.set_capability('sauce:options', sauce_options)
+url = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub"
+
+driver = webdriver.Remote(command_executor=url, options=options)
+
+
 
 def imprimir():
     
@@ -28,4 +44,15 @@ print(a)
 
 
 
+#https://www.guru99.com/introduction-to-selenium-grid.html
 
+options = ChromeOptions()
+options.browser_version = "122.0"
+options.platform_name = "Windows 10"
+lt_options = {};
+lt_options["username"] = "lucasoares3243";
+lt_options["accessKey"] = "MzXPbo5tmInEkPLKl3Dm9cca7Ia0P0sWDBtqlzvxeZGm1pbowH";
+lt_options["project"] = "Untitled";
+lt_options["w3c"] = True;
+lt_options["plugin"] = "python-python";
+options.set_capability('LT:Options', lt_options);
